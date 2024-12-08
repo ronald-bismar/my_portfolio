bd321d3db656a590004656f64ffcb4753e54f30a
Ronald Bismar <145393966+ronald-bismar@users.noreply.github.com>
2024-12-07 21:39:08 -0400

    Ántes de implementar el Iphone

--- lib/widgets/presentacion/widgets/cellphoneUI.dart
+++ lib/widgets/presentacion/widgets/cellphoneUI.dart
@@ -7,10 +7,10 @@ class CellPhoneUI extends StatefulWidget {
   const CellPhoneUI({super.key});
 
   @override
-  _CellphoneUIState createState() => _CellphoneUIState();
+  CellphoneUIState createState() => CellphoneUIState();
 }
 
-class _CellphoneUIState extends State<CellPhoneUI> {
+class CellphoneUIState extends State<CellPhoneUI> {
   Color colorPhone = const Color.fromARGB(255, 41, 41, 41);
   bool isSelected = false;
 
@@ -42,11 +42,8 @@ class _CellphoneUIState extends State<CellPhoneUI> {
         child: AnimatedContainer(
           padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
           duration: const Duration(milliseconds: 300),
-          width: isSelected
-              ? 400
-              : 260, // Ancho fijo (ajusta según el tamaño real del móvil que quieras imitar)
-          height:
-              560, // Alto fijo (ajusta según el tamaño real del móvil que quieras imitar)
+          width: isSelected ? 400 : 260,
+          height: 560,
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(28),
             color: colorPhone,
@@ -60,56 +57,12 @@ class _CellphoneUIState extends State<CellPhoneUI> {
 
   Widget secondLayerLight() {
     Color colorSombra = Colors.white;
-    List<BoxShadow> shadowsSecondLayer = [
-      BoxShadow(
-        color: colorSombra,
-        blurRadius: 2,
-      ),
-      BoxShadow(
-        color: colorSombra,
-        blurRadius: 2,
-      ),
-      BoxShadow(
-        color: colorSombra,
-        blurRadius: 2,
-      ),
-      BoxShadow(
-        color: colorSombra,
-        blurRadius: 2,
-      ),
-      BoxShadow(
-        color: colorSombra,
-        blurRadius: 2,
-      ),
-      BoxShadow(
-        color: colorSombra,
-        blurRadius: 2,
-      ),
-      BoxShadow(
-        color: colorSombra,
-        blurRadius: 2,
-      ),
-      BoxShadow(
-        color: colorSombra,
-        blurRadius: 2,
-      ),
-      BoxShadow(
-        color: colorSombra,
-        blurRadius: 2,
-      ),
-      BoxShadow(
-        color: colorSombra,
-        blurRadius: 2,
-      ),
-      BoxShadow(
-        color: colorSombra,
-        blurRadius: 2,
-      ),
-      BoxShadow(
-        color: colorSombra,
-        blurRadius: 2,
-      ),
-    ];
+    List<BoxShadow> shadowsSecondLayer = List.generate(
+        12,
+        (index) => BoxShadow(
+              color: colorSombra,
+              blurRadius: 2,
+            ));
 
     return AnimatedContainer(
       duration: const Duration(milliseconds: 300),
@@ -217,9 +170,7 @@ class _CellphoneUIState extends State<CellPhoneUI> {
                 "assets/images/wallpaper.jpg",
                 fit: BoxFit.cover,
               ),
-              Align(
-                  alignment: Alignment.topCenter,
-                  child: camera())
+              Align(alignment: Alignment.topCenter, child: camera())
             ],
           )),
     );
@@ -228,7 +179,6 @@ class _CellphoneUIState extends State<CellPhoneUI> {
   Widget camera() {
     return Transform(
       transform: Matrix4.identity()..rotateZ(pi / 4),
-// Suggested code may be subject to a license. Learn more: ~LicenseLog:613653895.
       origin: Offset.fromDirection(-0.3, 20),
       alignment: Alignment.center,
       child: Container(
@@ -241,39 +191,3 @@ class _CellphoneUIState extends State<CellPhoneUI> {
     );
   }
 }
-
-// class WavePainter extends CustomPainter { 
-// final double animationValue; // Animation value to control the wave height 
-
-// WavePainter(this.animationValue); 
-
-// @override 
-// void paint(Canvas canvas, Size size) { 
-// 	final paintGreen = Paint() 
-// 	..color = Colors.green // Green color for the wave 
-// 	..style = PaintingStyle.fill; // Filling the wave 
-
-// 	final pathGreen = Path(); 
-// 	pathGreen.moveTo(0, size.height); 
-// 	pathGreen.lineTo(0, size.height * 0.6); // Starting point for the wave 
-
-// 	for (var i = 0; i < size.width; i++) { 
-// 	final x = i.toDouble(); 
-// 	final y = size.height * 0.56 + 
-// 		animationValue * 
-// 			30 * 
-// 			sin((i / size.width) * 3 * pi); // Calculate wave shape 
-// 	pathGreen.lineTo(x, y); // Define wave path 
-// 	} 
-
-// 	pathGreen.lineTo(size.width, size.height); // Complete the wave path 
-// 	pathGreen.close(); 
-
-// 	canvas.drawPath(pathGreen, paintGreen); // Draw the green wave 
-// } 
-
-// @override 
-// bool shouldRepaint(covariant CustomPainter oldDelegate) { 
-// 	return true; // Repaint the wave continuously 
-// } 
-// }
